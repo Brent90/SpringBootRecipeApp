bdc62275c9f77ceee2c79a056e66a48ab8c14e6b
brent90 <brentjbaumgart@gmail.com>
2020-04-30 16:54:32 -0500

    Annotated pojos as @Entity and added @Id to all id instance variables.  closes #9

--- src/main/java/com/slinger/recipeapp/domain/Ingredient.java
+++ src/main/java/com/slinger/recipeapp/domain/Ingredient.java
@@ -4,14 +4,22 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.math.BigDecimal;
 
 @Getter
 @Setter
 @NoArgsConstructor
+@Entity
 public class Ingredient {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String description;
     private BigDecimal amount;
 
--- src/main/java/com/slinger/recipeapp/domain/Recipe.java
+++ src/main/java/com/slinger/recipeapp/domain/Recipe.java
@@ -4,12 +4,21 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 @Getter
 @Setter
 @NoArgsConstructor
+@Entity
 public class Recipe {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String description;
     private Integer prepTime;
     private Integer cookTime;
