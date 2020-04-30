a302b72e28641e3847db8a95f250b46fe0ac4f6a
brent90 <brentjbaumgart@gmail.com>
2020-04-30 14:27:42 -0500

    Add SpringBoot starter files

---
+++ src/main/java/com/slinger/recipeapp/SpringBootRecipeApplication.java
@@ -0,0 +1,13 @@
+package com.slinger.recipeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRecipeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRecipeApplication.class, args);
+    }
+
+}
